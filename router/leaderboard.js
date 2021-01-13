f405fc28712dac2cea8704178c787b8fa48ed7b8
Stephen-Roach <stephenroach21@gmail.com>
2021-01-12 18:52:06 -0600

    added changes to the server

---
+++ router/leaderboard.js
@@ -0,0 +1,11 @@
+module.exports = (app) => {
+  const db = require('../models');
+
+  // Display all users on the leaderboard
+  app.get('/leaderboard', async (req, res) => {
+    let allUsers = await db.User.findAll({
+      attributes: [userId, totalScore],
+    });
+    return allUsers;
+  });
+};
--- server.js
+++ server.js
@@ -3,33 +3,36 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const fetch = require('node-fetch');
-const cors = require('cors')
+const cors = require('cors');
 const passport = require('passport');
 
 const db = require('./models');
 
 const ensureAuthenticated = require('./middleware/ensureAuthenticated');
 const spotifyStrategy = require('./config/spotifyStrategy');
-const heartbeat = require("./router/heartBeat");
-const login = require('./auth/spotauth')
-const dashBoard = require('./api/dashboard')
-const spotifyToken = require('./api/spotifyToken')
+const heartbeat = require('./router/heartBeat');
+const login = require('./auth/spotauth');
+const dashBoard = require('./api/dashboard');
+const spotifyToken = require('./api/spotifyToken');
+const leaderboard = require('./api/leaderboard');
 
 const gitHubStrategy = require('./config/gitHubStrategy');
-const gitAuth = require('./auth/gitAuth')
+const gitAuth = require('./auth/gitAuth');
 
 const app = express();
 
-// // attach passport to express and sessions
-app.use(cors())
+// attach passport to express and sessions
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(session({
-    secret: "super secret",
+app.use(
+  session({
+    secret: 'super secret',
     keys: ['key1', 'key2'],
-    cookie: {maxAge: 60000}
-  }))
+    cookie: { maxAge: 60000 },
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -37,22 +40,22 @@ app.use(passport.session());
 passport.use(spotifyStrategy);
 // passport.use(gitHubStrategy);
 
-app.use('/', express.static(__dirname + '/public'))
-app.use('/js', express.static(__dirname + '/js'))
-app.use('/css', express.static(__dirname + '/css'))
+app.use('/', express.static(__dirname + '/public'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/css', express.static(__dirname + '/css'));
 
-app.get('/', (req,res) => {
-    res.json({
-        is:"MAIN PAGE"
-    })
+app.get('/', (req, res) => {
+  res.json({
+    is: 'MAIN PAGE',
+  });
 });
 
 heartbeat(app);
-login(app,passport);
+login(app, passport);
 // gitAuth(app,passport);
 dashBoard(app, ensureAuthenticated);
-spotifyToken(app,fetch)
-
+spotifyToken(app, fetch);
+leaderboard(app);
 
 app.listen(process.env.PORT, () => {
   console.log(`The server is running at port ${process.env.PORT}`);
