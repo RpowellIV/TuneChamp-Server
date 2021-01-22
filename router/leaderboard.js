694acc95919f5be85d0a2de612c358019052fcd6
Robert <r.powelliv@gmail.com>
2021-01-21 19:35:29 -0500

    woking

--- router/leaderboard.js
+++ router/leaderboard.js
@@ -1,14 +1,18 @@
+// const { all } = require('sequelize/types/lib/operators');
+
 module.exports = (app) => {
   const db = require('../models');
 
   // Display all users on the leaderboard
   app.get('/leaderboard', async (req, res) => {
+    console.log(`HERE`)
     let allUsers = await db.User.findAll({
       attributes: ['userId', 'totalScore'],
     });
 
     //sorts users by their score from greatest to least
     allUsers.sort((a, b) => (a.totalScore < b.totalScore ? 1 : -1));
+    
 
     res.json({
       is: 'working',
