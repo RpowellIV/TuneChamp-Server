d24c9a2bd9cccb9a994025341f981b41ca4ea908
Robert Powell <r.powelliv@gmail.com>
2021-01-23 19:29:38 -0500

    TEST

--- server.js
+++ server.js
@@ -20,24 +20,13 @@ const gitHubStrategy = require('./config/gitHubStrategy');
 const gitAuth = require('./auth/gitAuth');
 
 const app = express();
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
+const { Pool } = require('pg'); 
+const pool = new Pool({ 
+  connectionString: process.env.DATABASE_URL, 
+  ssl: { 
+    rejectUnauthorized: false 
+  } 
+}); 
 // attach passport to express and sessions
 app.use(cors());
 app.use(bodyParser.json());
