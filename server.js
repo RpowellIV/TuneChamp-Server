c8761bf6f24c8e70c4c85de620a0abea35820d57
Robert Powell <r.powelliv@gmail.com>
2021-01-23 19:39:18 -0500

    TEST

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const cors = require('cors');
 const passport = require('passport');
 
 const db = require('./models');
+var Sequelize = require('sequelize'), sequelize = null
+const http = require('http')
 
 const ensureAuthenticated = require('./middleware/ensureAuthenticated');
 const spotifyStrategy = require('./config/spotifyStrategy');
@@ -18,7 +20,14 @@ const leaderboard = require('./router/leaderboard');
 
 const gitHubStrategy = require('./config/gitHubStrategy');
 const gitAuth = require('./auth/gitAuth');
-
+// checks if env is Heroku, if so, sets sequelize to utilize the database hosted on heroku
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres'
+  })
+}
 const app = express();
 const { Pool } = require('pg'); 
 const pool = new Pool({ 
@@ -65,4 +74,14 @@ leaderboard(app);
 
 app.listen(process.env.PORT, () => {
   console.log(`The server is running at port ${process.env.PORT}`);
+});
+
+// at the bottom of your script, this sets your server to listen for requests, after sequelize has been synced.
+// so if you already have your server listening for requests, maybe delete that code. I think, IDK i just copied this
+// off of documentation
+// db.  is assuming you already set sequelize on db
+db.sequelize.sync().then(function() {
+  http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+  });
 });
